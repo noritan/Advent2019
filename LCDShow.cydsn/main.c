4a0070e3995d3873f3df1789c4f4381d54efdbdf
Nori Tan <github@noritan.org>
2019-11-23 07:50:35 +0900

    Created interim project
    Added I2CM and I2C_LCD components.
    Show character test pattern

--- LCDShow.cydsn/main.c
+++ LCDShow.cydsn/main.c
@@ -1,24 +1,27 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
+ * Copyright noritan.org, 2019
  * All Rights Reserved
  * UNPUBLISHED, LICENSED SOFTWARE.
  *
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * WHICH IS THE PROPERTY OF noritn.org.
  *
  * ========================================
 */
 #include "project.h"
 
-int main(void)
-{
+int main(void) {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    I2CM_Start();
+    I2C_LCD_Start();
+    
+    for (uint32_t i = 0x30; i <= 0x6F; i++) {
+        I2C_LCD_PutChar(i);
+    }
 
-    for(;;)
-    {
+    for (;;) {
         /* Place your application code here. */
     }
 }
